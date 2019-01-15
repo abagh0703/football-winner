6e83c82a519e97fd794270ea21314a6de9b46109
vicviod <vicviod@gmail.com>
2019-01-14 19:04:23 -0500

    new away and home goal models with documentation in linear_goal_model.py file on what the inputs are

--- models/linear_goal_model.py
+++ models/linear_goal_model.py
@@ -7,9 +7,24 @@ Created on Sun Nov 18 21:41:03 2018
 """
 
 import pandas as pd
+import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
+"""
+1. HAvP - Home average percentage chance (betting odds)
+2. DAvP - Draw average percentage chance (betting odds)
+3. AAvP - Away average percentage chance (betting odds)
+4. home_gls_game - home avg goals per game (season)
+5. away_gls_game - away avg goals per game (season)
+6. home_conceded_game - home abg goals conceded per game (season)
+7. away_conceded_game - away avg goals conceded per game (season)
+8. home_gls_game - home avg goals per game (last 20)
+9. away_gls_game - away avg goals per game (last 20)
+10. home_conceded_game - home abg goals conceded per game (last 20)
+11. away_conceded_game - away avg goals conceded per game (last 20)
+
+"""
 
 feature_cols = [26, 28, 30, 83, 84, 87, 88, 113, 114, 117, 118]
 label_col = 5
@@ -31,3 +46,6 @@ preds = reg.predict(X_test)
 preds = preds.round()
 
 difference = preds - y_test
+
+with open("home_goals.pkl", "wb") as f:
+    pickle.dump(reg, f) 
